46695c18f153ed8a777e348b858691999eb45a84
Paul Neenan <paulneenan@Mac.lan>
2025-11-10 22:01:50 -0500

    adjusts transparency of button

--- components/Header.tsx
+++ components/Header.tsx
@@ -68,7 +68,7 @@ export default function Header() {
               className={`font-semibold px-5 py-2.5 rounded-md transition border ${
                 scrolled
                   ? "bg-navy text-white border-navy hover:brightness-110"
-                  : "bg-white/50 text-navy border-navy hover:bg-navy hover:text-white"
+                  : "bg-white/60 text-navy border-navy hover:bg-navy hover:text-white"
               }`}
             >
               Schedule a Consultation
